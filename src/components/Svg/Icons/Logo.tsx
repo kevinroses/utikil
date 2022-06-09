43da87605068ef0d9225af7d159bbac6c37b367c
kevin <kevinxrturop@gmail.com>
2022-06-09 16:03:07 -0500

    first commit

---
+++ src/components/Svg/Icons/Logo.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Svg from "../Svg";
+import { SvgProps } from "../types";
+
+const Icon: React.FC<SvgProps> = (props) => {
+  return (
+   
+
+<svg version="1.2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 172 172"   {...props}>
+</svg>
+  );
+};
+
+export default Icon;
---
+++ src/widgets/Menu/config.ts
@@ -0,0 +1,156 @@
+export const links = [
+  {
+    label: "Home",
+    icon: "HomeIcon",
+    href: "/",
+  },
+  {
+    label: "Trade",
+    icon: "TradeIcon",
+    items: [
+      {
+        label: "Exchange",
+        href: "/",
+      },
+      {
+        label: "Liquidity",
+        href: "/",
+      },
+    ],
+  },
+  {
+    label: "Farms",
+    icon: "FarmIcon",
+    href: "/farms",
+  },
+  {
+    label: "Pools",
+    icon: "PoolIcon",
+    href: "/syrup",
+  },
+  {
+    label: "Lottery",
+    icon: "TicketIcon",
+    href: "/lottery",
+  },
+  {
+    label: "NFT",
+    icon: "NftIcon",
+    href: "/nft",
+  },
+  {
+    label: "Profile & Teams",
+    icon: "GroupsIcon",
+    items: [
+      {
+        label: "Leaderboard",
+        href: "/",
+      },
+      {
+        label: "YourProfile",
+        href: "/",
+      },
+    ],
+    calloutClass: "rainbow",
+  },
+  {
+    label: "Info",
+    icon: "InfoIcon",
+    items: [
+      {
+        label: "Overview",
+        href: "https://dominio.com",
+      },
+      {
+        label: "Tokens",
+        href: "https://dominio.com",
+      },
+      {
+        label: "Pairs",
+        href: "https://dominio.com",
+      },
+      {
+        label: "Accounts",
+        href: "https://dominio.com",
+      },
+    ],
+  },
+  {
+    label: "IFO",
+    icon: "IfoIcon",
+    items: [
+      {
+        label: "Next",
+        href: "/ifo",
+      },
+      {
+        label: "History",
+        href: "/ifo/history",
+      },
+    ],
+  },
+  {
+    label: "More",
+    icon: "MoreIcon",
+    items: [
+      {
+        label: "Voting",
+        href: "https://dominio.com",
+      },
+      {
+        label: "Github",
+        href: "https://dominio.com",
+      },
+      {
+        label: "Docs",
+        href: "https://dominio.com",
+      },
+      {
+        label: "Blog",
+        href: "https://dominio.com",
+      },
+    ],
+  },
+];
+
+export const socials = [
+  {
+    label: "Github",
+    icon: "GithubIcon",
+    href: "https://github.com/",
+  },
+  {
+    label: "Docs",
+    icon: "GitbookIcon",
+    href: "https://docs.dominio.com",
+  },
+  {
+    label: "Medium",
+    icon: "MediumIcon",
+    href: "https://dominio.com",
+  },
+  {
+    label: "Twitter",
+    icon: "TwitterIcon",
+    href: "https://twitter.com/",
+  },
+  {
+    label: "Telegram",
+    icon: "TelegramIcon",
+    items: [
+      {
+        label: "English",
+        href: "https://t.me/",
+      },
+      {
+        label: "Announcements",
+        href: "https://t.me/",
+      },
+    ],
+  },
+];
+
+export const MENU_HEIGHT = 64;
+export const MENU_ENTRY_HEIGHT = 48;
+export const SIDEBAR_WIDTH_FULL = 240;
+export const SIDEBAR_WIDTH_REDUCED = 56;
